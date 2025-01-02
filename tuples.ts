219d772ca47044f051d160ebf40df289609ff226
Bhavesh Panchal <bpanchal@atheerair.com>
2025-01-02 17:34:45 +0530

    Create tuples.ts

---
+++ tuples.ts
@@ -0,0 +1,12 @@
+let person : [string, number] = ['Bhavesh',25];
+console.log(person);
+
+let userName : string = person[0];
+let userAge : number = person[1];
+console.log(userName,userAge);
+
+person[0] = 'James';
+person[1] = 30;
+
+console.log(person);
+console.log(userName,userAge);
