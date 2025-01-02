672a849a5a0e0154060c260aa5b8f2b00205c7e0
Bhavesh Panchal <bpanchal@atheerair.com>
2025-01-02 15:32:53 +0530

    Demo 2

--- switchStatement.ts
+++ switchStatement.ts
@@ -1,26 +1,30 @@
-let day = 3;
+let day = 9;
 let dayName;
 
 switch (day) {
     case 1 :
-        dayName = "Sunday";
-        break;
-    case 2 :
         dayName = "Monday";
         break;
-    case 3:
+    case 2:
         dayName = "Tuesday";
         break;
-    case 4:
+    case 3:
         dayName = "Wednesday";
         break;
-    case 5:
+    case 4:
         dayName = "Thursday";
         break;
-    case 6:
+    case 5:
         dayName = "Friday";
         break;
-    case 7:
+    case 6:
         dayName = "Saturday";
         break;
+    case 7:
+        dayName = "Sunday";
+        break;
+    default:
+        dayName = "Weekend";
 }
+
+console.log(dayName);
